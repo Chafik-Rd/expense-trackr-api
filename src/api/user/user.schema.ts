2b93bb80b04842fdce66f9a368e336f94ba405a7
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-11-19 14:02:22 +0700

    fix(user): zod validation for user schemas

--- src/api/user/user.schema.ts
+++ src/api/user/user.schema.ts
@@ -3,16 +3,25 @@ import { getSchemaWithoutSchemaTag } from "../../utils/schema.js";
 
 // Login user
 export const loginUserSchema = zod.object({
-  email: zod.string().email(),
-  password: zod.string(),
+  email: zod.string().email().max(255, "Email cannot exceed 255 characters."),
+  password: zod
+    .string()
+    .min(6, "Password must be at least 6 characters.")
+    .max(64, "Password cannot exceed 64 characters."),
 });
 export const loginUserSchemaJSON = getSchemaWithoutSchemaTag(loginUserSchema);
 
 // Create user
 export const createUserSchema = zod.object({
   ...loginUserSchema.shape,
-  firstName: zod.string(),
-  lastName: zod.string(),
+  firstName: zod
+    .string()
+    .min(1, "First name is required.")
+    .max(50, "First name cannot exceed 50 characters."),
+  lastName: zod
+    .string()
+    .min(1, "Last name is required.")
+    .max(50, "Last name cannot exceed 50 characters."),
 });
 export const createUserSchemaJSON = getSchemaWithoutSchemaTag(createUserSchema);
 
